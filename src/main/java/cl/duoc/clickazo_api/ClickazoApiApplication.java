33148137a0f58e17ffa9e12b787a8eb388094ea9
JoaCortesP <Joa.CortesP@duocuc.cl>
2025-11-26 16:51:44 -0300

    Proyecto base Spring Boot para Clickazo

---
+++ src/main/java/cl/duoc/clickazo_api/ClickazoApiApplication.java
@@ -0,0 +1,13 @@
+package cl.duoc.clickazo_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ClickazoApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClickazoApiApplication.class, args);
+	}
+
+}
